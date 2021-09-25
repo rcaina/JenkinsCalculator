ae791e78ae320344e69a03ad9e5a694ddc7ccd51
rcaina <renzini2018@gmail.com>
2021-09-24 22:21:57 -0600

    continual program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,35 +10,32 @@ public class Main {
         System.out.println("Enter a command:\n");
         Scanner reader = new Scanner(System.in);
 
-        String z = reader.next();
-        int x = reader.nextInt();
-        if(reader.hasNext()) {
-            y = reader.nextInt();
-        }
+        while(reader.hasNext()) {
+            String z = reader.next();
+            int x = reader.nextInt();
+            if (!(z.equals("fibonnaci")) || !(z.equals("binary"))) {
+                y = reader.nextInt();
+            }
 
-        if(z.equals("add")) {
-            int a = calc.add(x, y);
-            System.out.println(a + "\n");
-        }
-        else if(z.equals("subtract")) {
-            int s = calc.subtract(x, y);
-            System.out.println(s + "\n");
-        }
-        else if(z.equals("divide")) {
-            int d = calc.divide(x, y);
-            System.out.println(d + "\n");
-        }
-        else if(z.equals("multiply")) {
-            int m = calc.multiply(x, y);
-            System.out.println(m + "\n");
-        }
-        else if(z.equals("fibonacci")) {
-            int f = calc.fibonacciNumberFinder(x);
-            System.out.println(f + "\n");
-        }
-        else if(z.equals("binary")) {
-            String i = calc.intToBinaryNumber(y);
-            System.out.println(i + "\n");
+            if (z.equals("add")) {
+                int a = calc.add(x, y);
+                System.out.println(a + "\n");
+            } else if (z.equals("subtract")) {
+                int s = calc.subtract(x, y);
+                System.out.println(s + "\n");
+            } else if (z.equals("divide")) {
+                int d = calc.divide(x, y);
+                System.out.println(d + "\n");
+            } else if (z.equals("multiply")) {
+                int m = calc.multiply(x, y);
+                System.out.println(m + "\n");
+            } else if (z.equals("fibonacci")) {
+                int f = calc.fibonacciNumberFinder(x);
+                System.out.println(f + "\n");
+            } else if (z.equals("binary")) {
+                String i = calc.intToBinaryNumber(y);
+                System.out.println(i + "\n");
+            }
         }
 
     }
