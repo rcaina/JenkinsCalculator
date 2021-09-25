53c04960e8dbbda70511af8fbf89bb1707bce9be
rcaina <renzini2018@gmail.com>
2021-09-24 22:33:17 -0600

    final program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args){
         Calculator calc = new Calculator();
-        int y = 0;
 
         System.out.println("Welcome to the calculator\n");
         System.out.println("Enter a command:\n");
@@ -12,29 +11,35 @@ public class Main {
 
         while(reader.hasNext()) {
             String z = reader.next();
-            int x = reader.nextInt();
-            if (!(z.equals("fibonacci")) || !(z.equals("binary"))) {
-                y = reader.nextInt();
-            }
+            if ((z.equals("fibonacci")) || (z.equals("binary"))) {
+                int t = reader.nextInt();
 
-            if (z.equals("add")) {
-                int a = calc.add(x, y);
-                System.out.println(a + "\n");
-            } else if (z.equals("subtract")) {
-                int s = calc.subtract(x, y);
-                System.out.println(s + "\n");
-            } else if (z.equals("divide")) {
-                int d = calc.divide(x, y);
-                System.out.println(d + "\n");
-            } else if (z.equals("multiply")) {
-                int m = calc.multiply(x, y);
-                System.out.println(m + "\n");
-            } else if (z.equals("fibonacci")) {
-                int f = calc.fibonacciNumberFinder(x);
-                System.out.println(f + "\n");
-            } else if (z.equals("binary")) {
-                String i = calc.intToBinaryNumber(y);
-                System.out.println(i + "\n");
+                if (z.equals("fibonacci")) {
+                    int f = calc.fibonacciNumberFinder(t);
+                    System.out.println(f);
+                } else if (z.equals("binary")) {
+                    String i = calc.intToBinaryNumber(t);
+                    System.out.println(i);
+                }
+            }
+            else {
+                int x = reader.nextInt();
+                int y = reader.nextInt();
+
+
+                if (z.equals("add")) {
+                    int a = calc.add(x, y);
+                    System.out.println(a);
+                } else if (z.equals("subtract")) {
+                    int s = calc.subtract(x, y);
+                    System.out.println(s);
+                } else if (z.equals("divide")) {
+                    int d = calc.divide(x, y);
+                    System.out.println(d);
+                } else if (z.equals("multiply")) {
+                    int m = calc.multiply(x, y);
+                    System.out.println(m);
+                }
             }
         }
 
