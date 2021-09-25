83d4d5c2c260ca2bc0dc95fa0f0090bb030d1cbb
rcaina <renzini2018@gmail.com>
2021-09-24 22:02:14 -0600

    add Main.java with function call and outputs

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        int y = 0;
+
+        System.out.println("Welcome to the calculator\n");
+        System.out.println("Enter a command:\n");
+        Scanner reader = new Scanner(System.in);
+
+        String z = reader.next();
+        int x = reader.nextInt();
+        if(reader.hasNext()) {
+            y = reader.nextInt();
+        }
+
+        if(z.equals("add")) {
+            int a = calc.add(x, y);
+            System.out.println(a + "\n");
+        }
+        else if(z.equals("subtract")) {
+            int s = calc.subtract(x, y);
+            System.out.println(s + "\n");
+        }
+        else if(z.equals("divide")) {
+            int d = calc.divide(x, y);
+            System.out.println(d + "\n");
+        }
+        else if(z.equals("multiply")) {
+            int m = calc.multiply(x, y);
+            System.out.println(m + "\n");
+        }
+        else if(z.equals("fibonacci")) {
+            int f = calc.fibonacciNumberFinder(x);
+            System.out.println(f + "\n");
+        }
+        else if(z.equals("binary")) {
+            String i = calc.intToBinaryNumber(y);
+            System.out.println(i + "\n");
+        }
+
+    }
+
+}
